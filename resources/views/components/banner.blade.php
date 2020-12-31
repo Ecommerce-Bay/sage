c6673992d479a621c8ee3962f50f1704faea598c
KellyVasquezZ <70528648+KellyVasquezZ@users.noreply.github.com>
2020-12-30 21:05:43 -0500

    Update slider principal

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,7 +1,79 @@
-<div class="banner-principal">
-    <p>World leading emission detection</p>
-    <h1 class="hello">{{$images}}</h1>
-    
-    <p>Separated they live in Bookmarksgrove right at the coast of the famous Semantics, large language</p>
-    <p>ocean and many more stuff and more more more</p>
+<div class="banner-principal swiper-container">
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+	<p>World leading emission detection</p>
+	<h1 class="hello">{{$images}}</h1>
+
+	<p>Separated they live in Bookmarksgrove right at the coast of the famous Semantics, large language</p>
+	<p>ocean and many more stuff and more more more</p>
 </div>
+<div class="swiper-slide">
+	<p>World leading emission detection</p>
+	<h1 class="hello">{{$images}}</h1>
+
+	<p>Separated they live in Bookmarksgrove right at the coast of the famous Semantics, large language</p>
+	<p>ocean and many more stuff and more more more</p>
+</div>
+</div>
+</div>
+<div class="banner-principal">
+
+<p>World leading emission detection</p>
+<h1 class="hello">{{$images}}</h1>
+
+<p>Separated they live in Bookmarksgrove right at the coast of the famous Semantics, large language</p>
+<p>ocean and many more stuff and more more more</p>
+</div>
+
+<!-- Swiper JS -->
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+<!-- Initialize Swiper -->
+<script>
+var swiper = new Swiper('.swiper-container');
+</script>
+<style>
+.swiper-container{
+	width: 100%;
+	height: 100%;
+}
+.swiper-slide {
+	text-align: center;
+	font-size: 18px;
+	background: #fff;
+	
+	/* Center slide text vertically */
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: -webkit-flex;
+	display: flex;
+	-webkit-box-pack: center;
+	-ms-flex-pack: center;
+	-webkit-justify-content: center;
+	justify-content: center;
+	-webkit-box-align: center;
+	-ms-flex-align: center;
+	-webkit-align-items: center;
+	align-items: center;
+
+
+	height: 100vh;
+	padding: 100px 10px;
+	justify-content: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: #f2f8ff;
+	color:#0065F2;
+	p{
+		margin: 0;
+		font-size: 20px;
+		font-weight: 400;
+	}
+	h1{
+		margin: 20px 0;
+		font-size: 50px;
+		font-weight: bold;
+	}
+}
+</style>
